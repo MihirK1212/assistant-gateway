35e387c0ab12c4b3c7fe82203fdaa8e1bddfc2e6
Mihir Karandikar <mihirkkarandikar@gmail.com>
2026-01-26 18:58:36 +0530

    adasD

--- src/python/assistant_gateway/chat_orchestrator/orchestration/__init__.py
+++ src/python/assistant_gateway/chat_orchestrator/orchestration/__init__.py
@@ -1,6 +1,6 @@
 from assistant_gateway.chat_orchestrator.orchestration.orchestrator import ConversationOrchestrator
 from assistant_gateway.chat_orchestrator.orchestration.agent_session_manager import AgentSessionManager
-from assistant_gateway.chat_orchestrator.orchestration.task_manager import TaskManager
+from assistant_gateway.chat_orchestrator.orchestration.task_manager import AgentTaskManager
 
-__all__ = ["AgentSessionManager", "ConversationOrchestrator", "TaskManager"]
+__all__ = ["AgentSessionManager", "ConversationOrchestrator", "AgentTaskManager"]
 
