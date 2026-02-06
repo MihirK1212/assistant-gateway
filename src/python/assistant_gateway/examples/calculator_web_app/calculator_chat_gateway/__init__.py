bb84bf1b1b2b12f4d841bc49ad1103821e065166
Mihir Karandikar <mihirkkarandikar@gmail.com>
2026-02-06 10:24:30 +0530

    asdasd

--- src/python/assistant_gateway/examples/calculator_web_app/calculator_chat_gateway/__init__.py
+++ src/python/assistant_gateway/examples/calculator_web_app/calculator_chat_gateway/__init__.py
@@ -11,9 +11,3 @@ python -m assistant_gateway.runner \
         --config assistant_gateway.examples.calculator_web_app.calculator_chat_gateway.gateway_config:config \
         --fastapi-only
 """
-
-
-'''
-chat 1: 7c7ea853-de65-4cb7-8e21-0bf40e5bbb42
-chat 2: 
-'''
